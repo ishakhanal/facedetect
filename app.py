027b00041bb28a220b3f02cf16e3a24437baef1d
isha <ishakhanal@hiupapp.com>
2019-09-17 10:49:31 +0545

    third

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import flask
 from flask import request, jsonify
+import cv2
+import os
+import base64
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,6 +12,15 @@ app.config["DEBUG"] = True
 def home():
     return '''<h1>Face Detection System</h1>
 <p>Welcome to ISPAM, we are here to server ai products to you</p>'''
+
+@app.route('/encodeimage', methods=['GET'])
+def encodingimage():
+	image = open('test1.jpg', 'rb')
+	image_read = image.read()
+	image_64_encode = base64.urlsafe_b64encode(image_read)
+
+	return image_64_encode
+
 
 if __name__=='__main__':
 	app.run()
