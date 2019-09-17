41af22f1492b34879de87595f3c95c349abaf872
isha <ishakhanal@hiupapp.com>
2019-09-17 11:14:10 +0545

    decodetry

--- app.py
+++ app.py
@@ -21,6 +21,36 @@ def encodingimage():
 
 	return image_64_encode
 
+@app.route('/decodeimage', methods=['POST'])
+def decodingimage():
+	
+	data = request.form
+	image_64_encode = data['es']
+
+
+	image_64_decode = base64.urlsafe_b64decode(image_64_encode)
+	image_result = open('testimage.jpg', 'wb')
+	image_result.write(image_64_decode)
+	
+	image = cv2.imread('testimage.jpg', 0)
+
+	face_detector = dlib.get_frontal_face_detector()
+
+	img = face_detector(image)
+
+	if img:
+
+		status = "detected"
+
+	else:
+
+		status = "undetected"
+
+	os.remove('testimage.jpg')
+
+
+	return status
+
 
 if __name__=='__main__':
 	app.run()
